8d9d152d88c8f883b756e100c8fc12917c84031e
marienov <senya2104@gmail.com>
2023-03-28 17:22:57 +0300

    fixed

--- lib/utils/include/utils.hpp
+++ lib/utils/include/utils.hpp
@@ -4,22 +4,25 @@
 #include <string>
 #include <set>
 #include <vector>
-using namespace std;
+
 
 struct Series {
 private:
     double rating;
-    string id;
-    string title;
+    std::string id;
+    std::string title;
 public:
-    Series(string id_, string title_, double rating_) : id(id_), title(title_), rating(rating_) {}
-    string getId() const { return id; }
-    string getTitle() const { return title; }
+    Series(std::string id_, std::string title_, double rating_) : id(id_), title(title_), rating(rating_) {}
+    std::string getId() const { return id; }
+    std::string getTitle() const { return title; }
     bool operator<(const Series& series) const {
         return (rating < series.rating);
     }
 };
-void choose_series(ifstream& file_bas, ifstream& file_rat, ifstream& file_title, int max_min, set<Series>* ser_set);
-void split_string(string& line, vector <string>* words);
+void choose_series(std::ifstream& file_bas, std::ifstream& file_rat, std::ifstream& file_title, int max_min, std::set<Series>& ser_set);
+void find_title(std::ifstream& file_title, std::string& s_id, std::string& s_title, std::string& last_line);
+double find_rating(std::ifstream& file_rat, std::string& s_id, std::string& last_line);
+void split_string(std::string const& line, std::vector <std::string>& words);
+bool isNumeric(std::string const& str);
+int input_params(int argc, char* argv[], std::ifstream& fbas, std::ifstream& faka, std::ifstream& frat );
 void print_set(std::set<Series> const& set_obj);
-bool isNumeric(string const& str);
